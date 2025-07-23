d9a410f1236c23dadc205632ebe146aa9cbeda58
BPerverseff <brad.perverseff@albertahealthservices.ca>
2025-07-23 13:48:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,5 +72,5 @@ import requests
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
